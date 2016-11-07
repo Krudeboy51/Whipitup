857b1c3e3c9dbc648d67f858b0e25ff0658037b8
Krudeboy51 <kking9351@gmail.com>
2016-11-07 13:04:34 -0500

    v1

---
+++ Whip it up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whip it up
+//
+//  Created by Kory E King on 11/7/16.
+//  Copyright © 2016 Kory E King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
